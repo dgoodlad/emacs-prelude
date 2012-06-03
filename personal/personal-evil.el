cbfc44e89837cab81824ec1f3e6f4ce5c7182c4a
David Goodlad <david@goodlad.net>
2012-06-04 09:46:08 +1000

    Add evil

---
+++ personal/personal-evil.el
@@ -0,0 +1,4 @@
+(evil-mode 1)
+(setq evil-shift-width 2)
+
+(provide 'personal-evil)
---
+++ personal/personal-packages.el
@@ -0,0 +1,6 @@
+(require 'prelude-packages)
+
+(add-to-list 'prelude-packages 'evil)
+(prelude-install-packages)
+
+(provide 'personal-packages)
