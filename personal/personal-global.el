9d59f3280a294e85878c6337d5c08239a835d83f
David Goodlad <david@goodlad.net>
2013-02-05 11:25:51 +1100

    Ensure zsh's PATH is respected

--- personal/personal-global.el
+++ personal/personal-global.el
@@ -1,1 +1,8 @@
 (scroll-bar-mode 0)
+
+;; read in PATH from .zshrc
+(if (not (getenv "TERM_PROGRAM"))
+    (setenv "PATH"
+            (shell-command-to-string "source $HOME/.zshrc && printf $PATH")))
+
+(provide 'personal-global)
