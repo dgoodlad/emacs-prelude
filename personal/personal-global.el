664aefeb3db43343967df556d01873c832f3f013
David Goodlad <david@goodlad.net>
2014-01-13 14:55:30 +1100

    Ensure ansi-term can render utf-8 characters

--- personal/personal-global.el
+++ personal/personal-global.el
@@ -5,4 +5,9 @@
     (setenv "PATH"
             (shell-command-to-string "source $HOME/.zshrc && printf $PATH")))
 
+;; ensure that ansi-term can render utf-8
+(defadvice ansi-term (after advise-ansi-term-coding-system)
+    (set-buffer-process-coding-system 'utf-8-unix 'utf-8-unix))
+(ad-activate 'ansi-term)
+
 (provide 'personal-global)
